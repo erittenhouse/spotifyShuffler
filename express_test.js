0d35a99095600185325faeae3804f694b77eefe4
erittenhouse <evanrittenhouse@gmail.com>
2020-11-25 14:12:47 -0800

    login working - working on exchanging login code for authorization token

--- express_test.js
+++ express_test.js
@@ -23,28 +23,14 @@ app.post('/login', (req, res) => {
     var scopes = 'user-read-private user-read-email';
     var redirectResponse = 'https://accounts.spotify.com/authorize?response_type=code&client_id=' + 'b817214b1ce74e28844cd890f5a050a3' +
         (scopes ? '&scope=' + encodeURIComponent(scopes) : '') + '&redirect_uri=' + encodeURIComponent(redirectURI)
-    
     axios.get(redirectResponse)
         .then(function (response) {
-            console.log(response);
-        })
-    res.redirect(redirectResponse)
+            res.redirect(redirectResponse)
+        });
 
     
-    /* var url = getFormattedUrl(res)
-    console.log(url)
-    var accessCode = new URLSearchParams(url.search).get('code');
-    console.log(accessCode)
-
-    const data = {
-        grant_type: 'authorization_code', 
-        code: accessCode,
-        redirect_uri: redirectURI
-    };
-    axios.post('https://accounts.spotify.com/api/token', data);
-    console.log('axios request sent') */
-
 });
+
 
 app.use('/test-form?code=*', (req, res) => {
     console.log('accessed');
@@ -55,7 +41,7 @@ app.get('/test-form', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-    res.sendFile('/test.html', options)
+    res.sendFile('/login.html', options)
 })
 
 app.listen(port, () => {
