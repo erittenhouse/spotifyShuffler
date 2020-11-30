d4677a8443f57c1aa054f984820e12fa8188c1a7
erittenhouse <evanrittenhouse@gmail.com>
2020-11-29 20:25:01 -0800

    got access token - have to figure out refresh part as well as how to hide the client ID/client secret

--- express_test.js
+++ express_test.js
@@ -1,50 +1,89 @@
-const fetch = require('node-fetch')
-const urllib = require('url')
+const fetch = require('node-fetch');
+const urllib = require('url');
 const express = require('express');
 const app = express();
-const redirectURI = 'http://localhost:5000/test-form.html';
 const port = 5000;
 const bodyParser = require("body-parser");
 const { stringify } = require('querystring');
+
+const clientId = 'b817214b1ce74e28844cd890f5a050a3';
+const clientSecret = '6a74579918844fbbb727d9f56e4883a8';
+const encodedId = 'YjgxNzIxNGIxY2U3NGUyODg0NGNkODkwZjVhMDUwYTM=';
+const encodedSecret = 'NmE3NDU3OTkxODg0NGZiYmI3MjdkOWY1NmU0ODgzYTg=';
+const redirectURI = 'http://localhost:5000/shuffle/';
 var options = { root: __dirname };
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname))
 
-// app.post('/login', (req, res) => {
-//     var scopes = 'playlist-modify-public playlist-modify-private playlist-read-private playlist-read-collaborative';
-//     var redirectResponse = 'https://accounts.spotify.com/authorize?response_type=code&client_id=' + 'b817214b1ce74e28844cd890f5a050a3' +
-//         (scopes ? '&scope=' + encodeURIComponent(scopes) : '') + '&redirect_uri=' + encodeURIComponent(redirectURI);
-//     axios.get(redirectResponse)
-//         .then(function (response) {
-//             console.log(response);
-//             res.redirect(redirectResponse);
-//         });
-// }); 
+// helper function to generate random string.
+var generateRandomString = function(length) { 
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
+    for (var i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    };
+
+    return text;
+};
+
+// main page
 app.get('/', (req, res) => {
     res.sendFile('/index.html', options)
 })
 
-// this is the one that I'm using to test, the above one is the one that actually works
+// login page - triggered on click of button, redirects to spotify then goes to /shuffle (b/c /shuffle is the redirect URI)
 app.get('/login', (req, res) => {
-    var redirectURI = 'http://localhost:5000/test-form.html';
+    var state = generateRandomString(16);
+    var stateKey = 'spotify_auth_state';
+    res.cookie(stateKey, state);
+
     let scopes = 'playlist-modify-private playlist-modify-public playlist-read-private playlist-read-collaborative';
     let redirectResponse = 'https://accounts.spotify.com/authorize?response_type=code&client_id=' + 'b817214b1ce74e28844cd890f5a050a3' +
-                (scopes ? '&scope=' + encodeURIComponent(scopes) : '') + '&redirect_uri=' + encodeURIComponent(redirectURI);
-    fetch(redirectResponse)
-    res.redirect(redirectResponse);
+                (scopes ? '&scope=' + encodeURIComponent(scopes) : '') + '&redirect_uri=' + encodeURIComponent(redirectURI) + '&state=' + state;
+    
+    res.redirect('https://accounts.spotify.com/authorize?' + 
+        stringify({
+            response_type: 'code',
+            client_id: clientId, 
+            client_secret: clientSecret,
+            scope: scopes,
+            redirect_uri: redirectURI,
+            state: state
+        }));
 });
 
-app.get('/test-form*', (req, res) => {
-    console.log('app.get testform')
-    var code = (req.query.code != null) ? req.query.code: null; // if req.query.code is not null, code = req.query.code else null
-    var state = (req.query.state != null) ? req.query.state: null;
+// main page. Has the shuffle button and sends the access token, etc. to spotify backend
+app.get('/shuffle', (req, res) => {
+    var code = (req.query.code != null) ? req.query.code : null;  // if req.query.code is not null, code = req.query.code else null
+    var state = (req.query.state != null) ? req.query.state : null;  // if req.query.state is not null, state = req.query.state else null
 
-    console.log(req);
-    console.log('state: ' + String(state));
-    console.log('code: ' + String(code));
-    res.sendFile('/test-form.html');
+    console.log('Code: ' + code);
+    console.log('State: ' + state);
+
+    async function getAuthToken() { 
+        const request = fetch('https://accounts.spotify.com/api/token', {
+            method: 'POST', 
+            headers: { 
+                'Authorization': 'Basic ' + (new Buffer.from(clientId + ':' + clientSecret).toString('base64')),
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: stringify({
+                "grant_type": "authorization_code",
+                "code": code,
+                "redirect_uri": redirectURI
+            })
+        });
+        let response = await request;
+        let token = await response.json();
+        console.log('Access_Token: ' + token.access_token);
+        console.log('Expires in: ' + token.expires_in);
+        return token;
+    };
+
+    getAuthToken();
+    res.send('test');
 });
 
 app.listen(port, () => {
