314948e4734f5d596d9e655c7a350b1dfa87a90e
erittenhouse <evanrittenhouse@gmail.com>
2020-12-06 13:36:48 -0800

    can get preliminary list of user playlists

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ var options = { root: __dirname };
 const app = express();
 const port = 5000;
 const bodyParser = require("body-parser");
+const { response } = require('express');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname))
 
@@ -38,8 +39,6 @@ app.get('/login', (req, res) => {
     res.cookie(stateKey, state);
 
     let scopes = 'playlist-modify-private playlist-modify-public playlist-read-private playlist-read-collaborative';
-    let redirectResponse = 'https://accounts.spotify.com/authorize?response_type=code&client_id=' + 'b817214b1ce74e28844cd890f5a050a3' +
-                (scopes ? '&scope=' + encodeURIComponent(scopes) : '') + '&redirect_uri=' + encodeURIComponent(redirectURI) + '&state=' + state;
     
     res.redirect('https://accounts.spotify.com/authorize?' + 
         stringify({
@@ -54,39 +53,81 @@ app.get('/login', (req, res) => {
 
 // main page. Has the shuffle button and sends the access token, etc. to spotify backend
 app.get('/shuffle', (req, res) => {
+    playlists = []
     var code = (req.query.code != null) ? req.query.code : null;  // if req.query.code is not null, code = req.query.code else null
     var state = (req.query.state != null) ? req.query.state : null;  // if req.query.state is not null, state = req.query.state else null
 
-    console.log('Code: ' + code);
-    console.log('State: ' + state);
-
     const getAuthToken = async () => { 
-        const request = await fetch('https://accounts.spotify.com/api/token', {
+        var accessReq = await fetch('https://accounts.spotify.com/api/token', {
             method: 'POST', 
             headers: { 
                 'Authorization': 'Basic ' + (new Buffer.from(clientId + ':' + clientSecret).toString('base64')),
                 'Content-Type': 'application/x-www-form-urlencoded'
             },
             body: stringify({
-                'grant_type': 'authorization_code',
-                'code': code,
-                'redirect_uri': redirectURI
+                grant_type: 'authorization_code',
+                code: code,
+                redirect_uri: redirectURI
             })
         });
-
-        let response = await request;
-        console.log(response);
-        return response;
+        var accessJson = accessReq.json();
+        return accessJson;
     };
 
-    var token = getAuthToken();  // returns response object
-    token.then( (response) => response.json() )  // returns response JSON - a Promise, in this case.
-        .then( responseJson => { console.log(responseJson.access_token) });  // prints response JSON. this line and line 
-                                                                             // 83 are the ONLY TWO VALID implementations
-                                                                             // w/ brackets: no parentheses/{} is invalid
-        //  lines 83 and 84 
-    
+    var access = getAuthToken();  // returns Promise object
+    access.then( async (accessJson) => {
+        tokens = {
+            'access_token': accessJson.access_token,
+            'refresh_token': accessJson.refresh_token,
+            'expires_in': accessJson.expires_in
+        }
+        console.log(tokens);
 
+        var userIdReq = await fetch('https://api.spotify.com/v1/me', {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${tokens.access_token}`
+            }
+        })
+        var userIdJson = userIdReq.json();
+        return [tokens, userIdJson]
+    } ).then ( async (tokensAndId) => {
+        access = tokensAndId[0].access_token
+        userId = tokensAndId[1] 
+
+        var playlistReq = await fetch('https://api.spotify.com/v1/me/playlists', {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${access}`
+            }
+        }).then( playlistReq => playlistReq.json() )
+        .then( playlistJson => console.log(playlistJson) )  // playlists contained in playlistJson.items
+
+    } )
+    // token.then( (response) => console.log(response) )  // when Promise is resolved, return the JSON
+        /* .then( (responseJson) => [responseJson.access_token, responseJson.refresh_token, responseJson.expires_in] )
+        .then( async (responseTokens) => { 
+            console.log('Access Token: ' + responseTokens[0])
+            console.log('Refresh Token: ' + responseTokens[1])
+
+            var idReq = await fetch('https://api.spotify.com/v1/me', {
+                method: 'GET', 
+                headers: {
+                    'Authorization': 'Bearer ' + responseTokens[0]
+                }
+            })
+         } );
+        .then( async (responseJson) => { 
+            console.log(responseJson);
+            const userData = await fetch('https://api.spotify.com/v1/me', {
+                method: 'GET', 
+                headers: {
+                    'Authorization': 'Bearer ' + responseJson.access_token
+                }
+            });
+            return userData;
+        }) 
+        .then( userData => console.log(userData.json()))*/
 });
 
 app.listen(port, () => {
